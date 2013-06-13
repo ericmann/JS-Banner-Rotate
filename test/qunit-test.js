670734444cee45bba7e4648cc31eeb4463c0919a
Eric Mann <eric@eamann.com>
2013-06-12 17:08:08 -0700

    Add another test

--- test/qunit-test.js
+++ test/qunit-test.js
@@ -15,10 +15,31 @@ test( 'images are preloaded', function() {
 	// Act
 	rotator.preloadImages();
 	
+	// Verify
 	equal( rotator.preloaded.length, 2 );
 	
 	equal( typeof rotator.preloaded[0], 'object' );
 	equal( typeof rotator.preloaded[1], 'object' );
 	equal( rotator.preloaded[0].src, images[0] );
 	equal( rotator.preloaded[1].src, images[1] );
+} );
+
+test( 'top image replaces bottom', function() {
+	expect(1);
+	
+	// Set up
+	var rotateDiv = document.getElementById( 'rotating-images' ),
+		top = rotateDiv.querySelector( '.top-layer' ),
+		bottom = rotateDiv.querySelector( '.bottom-layer' );
+		
+	top.style.backgroundImage = 'url(http://events.jquery.org/2013/portland/img/logo@2x.png)';
+	bottom.style.backgroundImage = 'none';
+	
+	var rotator = new window.ImageRotator( { images: images } );
+	
+	// Act
+	rotator.fadeImage();
+	
+	// Verify
+	equal( bottom.style.backgroundImage, 'url(http://events.jquery.org/2013/portland/img/logo@2x.png)' );
 } );
